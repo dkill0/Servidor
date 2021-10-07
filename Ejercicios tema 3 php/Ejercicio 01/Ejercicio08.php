6831cee6c8cb56a6baf27a5690db867992bb8c60
dkill0 <cpalonso98@hotmail.com>
2021-10-07 21:10:04 +0200

    s

--- Ejercicios tema 3 php/Ejercicio 01/Ejercicio08.php
+++ Ejercicios tema 3 php/Ejercicio 01/Ejercicio08.php
@@ -8,7 +8,17 @@
 </head>
 <body>
     <?php
-
+    $x = -1;
+    $y = 9;
+    //Les faltaba el $ que los declara como variables
+    $suma = $x + $y;
+    //Le faltaba el ';', el <br> se coloca dentro del print y se escribe asi
+    print ("El valor de x es <i>$x</i><br>");
+    
+    //Le faltaba el ')' y el <br>
+    print("El valor de y es <i>$y</i> <br>");
+    //El <br> estaba mal escrito
+    print("La suma es <b><i>$suma</i></b><br>");
     ?>
 
 
