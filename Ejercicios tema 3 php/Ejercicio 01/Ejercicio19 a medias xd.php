037163ea6e2bca15970f43abb5ce32e1e341e407
dkill0 <cpalonso98@hotmail.com>
2021-10-08 12:23:02 +0200

    s

--- Ejercicios tema 3 php/Ejercicio 01/Ejercicio19 a medias xd.php
+++ Ejercicios tema 3 php/Ejercicio 01/Ejercicio19 a medias xd.php
@@ -9,9 +9,14 @@
 <body>
 <?php
  for ($i=0; $i <= 6 ; $i++) { 
-    for ($j=0; $j <=$i ; $j++) { 
+    for ($j=0; $j <$i ; $j++) { 
+        echo (" ");
+     }
+    for ($j=6; $j > $i ; $j--) { 
        echo ("*");
     }
+
+  
     echo("<br>");
 }
 
