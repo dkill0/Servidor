c2ca9ed3517af1cb8e84f43edfa83bf60e736524
dkill0 <cpalonso98@hotmail.com>
2021-10-08 12:57:58 +0200

    sw

--- Ejercicios tema 3 php/Ejercicio 01/Ejercicio19.php
+++ Ejercicios tema 3 php/Ejercicio 01/Ejercicio19.php
@@ -8,30 +8,32 @@
 </head>
 <body>
 <h3>Introduce la altura de la piramide</h3>
-<form method="post" action="Ejercicio18.php">
+<form method="post" action="Ejercicio19.php">
     <input name="num1" type="text" maxlength="30" size="40">
     <br>
     <input type="submit" value="Enviar"> 
     <input type="reset" value="Borrar"> 
     <br><br>
 </form>
-    <?php
-    $numero1=0;
+<?php
+ $numero1=0;
  
-    if(isset ($_POST['num1']) && !empty($_POST['num1'])){
-       $numero1= $_POST['num1']; 
-   
-   }
-
-    for ($i=0; $i <= $numero1; $i++) { 
-        for ($j=0; $j <=$i ; $j++) { 
-           echo ("*");
-        }
-        echo("<br>");
+  if(isset ($_POST['num1']) && !empty($_POST['num1'])){
+     $numero1= $_POST['num1']; 
+ 
+ }
+ for ($i=0; $i <= $numero1 ; $i++) { 
+    for ($j=0; $j <$i ; $j++) { 
+        echo (" ");
+     }
+    for ($j=$numero1; $j > $i ; $j--) { 
+       echo ("*");
     }
 
+  
+    echo("<br>");
+}
 
 ?>
-    
 </body>
 </html>
