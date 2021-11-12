c3f45525651f33348c0dd1c9fb4a176d83d79cde
dkill0 <cpalonso98@hotmail.com>
2021-11-12 12:24:16 +0100

    gf

---
+++ Restaurante/camarero/headerCam.php
@@ -0,0 +1,14 @@
+<header>
+        <h1>Lentejas los jueves</h1>
+        <figure>
+            <img src="" alt="">
+        </figure>
+    </header>
+    <nav>
+    <ul>
+        <li><a href="mesasCam.php">Mesas</a></li>
+        <li><a href="pedidosCam.php">Pedidos</a></li>
+        <li><a href="areaPCam.php">Area Personal</a></li>
+    </ul>
+
+</nav>
--- Restaurante/camarero/mesasCam.php
+++ Restaurante/camarero/mesasCam.php
@@ -1,22 +1,13 @@
-<?php
-    include("../Seguridad.php");
-?>
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lentejas los Jueves</title>
+    <title>Mesas</title>
     <link href="css/EstilosIndex.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 <?php include("headerCam.php");?>
-    <?php
-        echo "Holaaaaaaa camarero";
-        echo '<br>';
-        echo $_SESSION['name'];
-        ?>
 </body>
 </html>
