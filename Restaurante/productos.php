8efb5452d4995c3bbabbf8daa5ad9c88da07a934
dkill0 <cpalonso98@hotmail.com>
2021-11-26 08:50:34 +0100

    Update productos.php

--- Restaurante/productos.php
+++ Restaurante/productos.php
@@ -64,15 +64,29 @@
           
           while($row = mysqli_fetch_array($result)){
               
-              
-            print('<div class=" rounded mt-3 mb-3 m-auto prod col-11 col-md-4 col-lg-5">');
-                print("<h3>".$row['nombre']."</h3>");
-                print("<h6>Ingredientes</h6>");
-                print("<p>".$row['descripcion']."</p>");
-                print('<img class="rounded float-end img-fluid"src=.'.$row['imagen'].'>');
-                print("<p class=text-end>".$row['precio']."€</p>");
-                print("</div>");
-              }
+
+            print('<div class="rounded mt-3 mb-3 m-auto prod col-auto" style="width: 400px;"> ');
+
+
+            print('<h2 class="fw-bold text-center">');
+            print($row['nombre']."</h2>");
+            
+            print("<div class=row>");
+            print("<h6>Ingredientes</h6>");
+            print('<p class="mb-4" style="height: 50px;">');
+            print($row['descripcion']."</p>");
+            print("</div>");
+            
+            
+            print('<img class=" m-auto row rounded float-end img-fluid" style="width: 400px; height: 250px;" src=.'.$row['imagen'].'>');
+           
+            
+            print("<div class=row>");
+            print("<p class=text-end>".$row['precio']."€</p>");
+            print("</div>");
+            print("</div>");
+            
+          }
               
           
     ?>
@@ -97,14 +111,29 @@
           
           while($row = mysqli_fetch_array($result)){
               
-              
-            print('<div class=" rounded mt-3 mb-3 m-auto prod col-11 col-md-4 col-lg-5">');
-                print("<h3>".$row['nombre']."</h3>");
-                print("<h6>Ingredientes</h6>");
-                print("<p>".$row['descripcion']."</p>");
-                print("<footer class=text-end>".$row['precio']."€</footer>");
-                print("</div>");
-              }
+
+            print('<div class="rounded mt-3 mb-3 m-auto prod col-auto" style="width: 400px;"> ');
+
+
+            print('<h2 class="fw-bold text-center">');
+            print($row['nombre']."</h2>");
+            
+            print("<div class=row>");
+            print("<h6>Ingredientes</h6>");
+            print('<p class="mb-4" style="height: 50px;">');
+            print($row['descripcion']."</p>");
+            print("</div>");
+            
+            
+            print('<img class=" m-auto row rounded float-end img-fluid" style="width: 400px; height: 250px;" src=.'.$row['imagen'].'>');
+           
+            
+            print("<div class=row>");
+            print("<p class=text-end>".$row['precio']."€</p>");
+            print("</div>");
+            print("</div>");
+            
+          }
               
           
     ?>
@@ -126,18 +155,31 @@
           echo mysqli_error($conn);
           
           
-                  
-          
           while($row = mysqli_fetch_array($result)){
               
-              
-                print('<div class=" rounded mt-3 mb-3 m-auto prod col-11 col-md-4 col-lg-5">');
-                print("<h3>".$row['nombre']."</h3>");
-                print("<h6>Descripción</h6>");
-                print("<p>".$row['descripcion']."</p>");
-                print("<p class=text-end>".$row['precio']."€</p>");
-                print("</div>");
-              }
+
+            print('<div class="rounded mt-3 mb-3 m-auto prod col-auto" style="width: 400px;"> ');
+
+
+            print('<h2 class="fw-bold text-center">');
+            print($row['nombre']."</h2>");
+            
+            print("<div class=row>");
+            print("<h6>Ingredientes</h6>");
+            print('<p class="mb-4" style="height: 50px;">');
+            print($row['descripcion']."</p>");
+            print("</div>");
+            
+            
+            print('<img class=" m-auto row rounded float-end img-fluid" style="width: 400px; height: 250px;" src=.'.$row['imagen'].'>');
+           
+            
+            print("<div class=row>");
+            print("<p class=text-end>".$row['precio']."€</p>");
+            print("</div>");
+            print("</div>");
+            
+          }
               
           
     ?>
