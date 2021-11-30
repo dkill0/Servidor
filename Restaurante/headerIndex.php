6f68de270fce1b75e8a75e4dc0314097f753cfe0
dkill0 <cpalonso98@hotmail.com>
2021-11-30 19:09:28 +0100

    d

--- Restaurante/headerIndex.php
+++ Restaurante/headerIndex.php
@@ -3,21 +3,38 @@
     <div class=" col-12 text-uppercase text-center">
     <h1 class="display">Lentejas los jueves</h1>
     </div>
-    
-    <div class="col-12 text-end iniS">
-        <a class="iniS me-2" href="inicioSes.php">Iniciar sesión</a>
-        <a class="iniS me-2"  href="registroCli.php">Registrarse</a>
-    </div>
+
 </header>
    
-<nav class="navbar navbar-expand sticky-top navbar-dark bg-dark">
-        <div class="container-fluid justify-content-start">
-            <a class="navbar-brand"href="index.php"><img class="rounded" src="./img/logoLent.png"></a>
-        <ul class="navbar-nav">
-            <li class="nav-item"><a class="nav-link " href="index.php">Inicio</a></li>
-            <li class="nav-item"><a class="nav-link " href="cartaIndex.php">Carta</a></li>
-            <li class="nav-item"><a class="nav-link " href="contactoIndex.php">Contacto</a></li>
-        </ul>
 
-        </div>
-    </nav>
+    <nav class="navbar sticky-top navbar-expand-sm navbar-dark bg-dark">
+					
+		
+                    <div class="container-fluid justify-content-between">
+                        
+            
+                        
+                        <a class="navbar-brand" href="#">
+                            <img src="./img/logoLent.png" alt="" style="width:40px">
+                        </a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar_plegable">
+                            <span class="navbar-toggler-icon"></span>
+                        </button> 
+                        <div class="collapse navbar-collapse" id="navbar_plegable">
+                            <ul class="navbar-nav me-auto text-end">
+                            <li class="nav-item">
+                                <a class="nav-link link-active" href="index.php">Inicio</a>
+                            </li>
+                            <li class="nav-item"><a class="nav-link"href="cartaIndex.php">Carta</a></li>
+                            <li class="nav-item"><a class="nav-link" href="contactoIndex.php">Contacto</a></li>
+                        </ul>
+                        <br>
+                            <div class="d-flex justify-content-around">
+                            <a class="me-2 text-end link-light" href="inicioSes.php">Iniciar sesión</a>
+                            <a class="me-2 text-end link-light"  href="registroCli.php">Registrarse</a>
+                
+                            </div>
+                        </div>
+                        
+                    </div>
+                </nav>
