131cc9a4ec4d2eb823e94e2967b761cce14a46ff
dkill0 <cpalonso98@hotmail.com>
2022-02-04 14:30:20 +0100

    fghj

---
+++ Vegazoon/laravel/resources/views/products/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.main')
+@section('contenido')
+hola
+
+@endsection
--- Vegazoon/laravel/routes/web.php
+++ Vegazoon/laravel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+
+    Route::get('products', 'ProductController@index')->name('products.index');
+
+    Route::get('products/create', 'ProductController@create')->name('products.create');
+
+    Route::post('products', 'ProductController@store')->name('products.store');
+
+    Route::delete('products/{id}', 'ProductController@destroy')->name('products.destroy');
+
+    Route::get('products/{id}/edit', 'ProductController@edit')->name('products.edit');
+
+    Route::put('products/{id}', 'ProductController@update')->name('products.update');
+
+    Route::get('/activity', 'ProductController@updatedActivity');
 });
+
+
+Auth::routes();
+
