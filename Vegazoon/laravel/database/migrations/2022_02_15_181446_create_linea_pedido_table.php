34d2cc1f2f692a02fcfdb187f66d22012dde363c
dkill0 <cpalonso98@hotmail.com>
2022-02-15 19:14:56 +0100

    f

--- Vegazoon/laravel/database/migrations/2022_02_15_181446_create_linea_pedido_table.php
+++ Vegazoon/laravel/database/migrations/2022_02_15_181446_create_linea_pedido_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePedidoTable extends Migration
+class CreateLineaPedidoTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class CreatePedidoTable extends Migration
      */
     public function up()
     {
-        Schema::create('pedido', function (Blueprint $table) {
-            $table->increments('idPedido');
-            $table->unsignedBigInteger('usuario');
+        Schema::create('linea_pedido', function (Blueprint $table) {
+            $table->id();
             $table->timestamps();
-            $table->foreign("usuario")
-                    ->references("id")
-                    ->on("users");
         });
     }
 
@@ -30,6 +26,6 @@ class CreatePedidoTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('pedido');
+        Schema::dropIfExists('linea_pedido');
     }
 }
